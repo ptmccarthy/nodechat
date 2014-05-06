926cb3713e9ae35789e1f9ff360c56da8ddb2a34
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-05-06 00:17:19 -0700

    Implement winston for logging

--- config/config_server.js
+++ config/config_server.js
@@ -3,7 +3,13 @@ var config = {}
 // general settings
 config.port = '8000';
 config.mongoURL = 'localhost:27017/chat_db';
-config.logLevel = 2; // 0 - error, 1 - warn, 2 - info, 3 - debug
+
+// logging settings
+// for lvels use standard syslog severity levels
+config.consoleLogLevel = 'info'
+config.fileLogLevel = 'debug'
+config.logFile = './logs/ddapp.log'
+config.maxLogFileSize = 4194304 // bytes
 
 // application settings
 config.displayRecent = 10;
---
+++ logger.js
@@ -0,0 +1,18 @@
+var config = require('./config/config_server')
+
+// external libraries
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      level: config.consoleLogLevel
+    }),
+    new (winston.transports.File)({ 
+      level: config.fileLogLevel,
+      filename: config.logFile
+    })
+  ]
+});
+
+module.exports = logger;
