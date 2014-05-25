6410599a13703abd12e0045c4c7586815a4fca29
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2014-05-25 11:03:39 -0700

    added socket manager so that we can easily get a list of sockets attached to a particular user. these don't have to be initiated from a single device

--- sockets.js
+++ sockets.js
@@ -11,6 +11,31 @@ var chats = db.get('history');
 
 var io;
 
+var socketManager = {
+  register: function(socket) {
+    var socketUser = socket.manager.handshaken[socket.id].user;
+    // use this label with appended 'user-' so that we don't
+    // have collisions with users and method names
+    var userLabel = 'user-' + socketUser.username;
+    if (!this[userLabel]) {
+      this['user-' + socketUser.username] = [socket];
+    } else {
+      this[userLabel].push(socket);
+    }
+  },
+
+  unregister: function(socket) {
+    var socketUser = socket.manager.handshaken[socket.id].user;
+    var userLabel = 'user-' + socketUser.username;
+    var socketsArr = this[userLabel];
+    for (var i = 0; i < socketsArr.length; i++) {
+      if (socketsArr[i].id === socket.id) {
+        socketsArr.splice(i, 1);
+      }
+    }
+  }
+};
+
 module.exports = function(sio, passport, sessionStore) {
   io = sio;
   io.sockets.on('connection', onConnect);
@@ -24,18 +49,8 @@ module.exports = function(sio, passport, sessionStore) {
   }));
 }
 
-var authSuccess = function(data, accept) {
-  logger.info("Socket initiated by user " + data.user.username);
-  accept(null, true);
-}
-
-var authFailure = function(data, message, error, accept) {
-  if (error) throw new Error(message);
-  logger.info("Socket auth failed: " + message);
-  accept(null, false);
-}
-
 var onConnect = function (socket) {
+  socketManager.register(socket);
   socket.emit('message', {
     message: 'Connected to chat. Displaying ' + config.displayRecent + ' most recent messages...',
     timestap: moment()
@@ -45,6 +60,7 @@ var onConnect = function (socket) {
   initializeListeners(socket);
 
   socket.on('disconnect', function() {
+    socketManager.unregister(socket);
     logger.info('Socket disconnected. ID: ' + socket.id);
   });
 }
@@ -63,4 +79,15 @@ var sendRecentHistory = function (socket) {
       socket.emit('message', doc[doc.length-i]);
     }
   });
+}
+
+var authSuccess = function(data, accept) {
+  logger.info("Socket initiated by user " + data.user.username);
+  accept(null, true);
+}
+
+var authFailure = function(data, message, error, accept) {
+  if (error) throw new Error(message);
+  logger.info("Socket auth failed: " + message);
+  accept(null, false);
 }
