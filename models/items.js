83630319e1dbae5ed739c760d3f64257337a115f
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2014-06-07 17:18:56 -0700

    added basic item model and inventory and gold to the character model. The item model has a 'template' flag which can define prototype items that can't be added to a player inventory directly, but can be used to generate copy instances of the item

---
+++ models/items.js
@@ -0,0 +1,23 @@
+var config = require('../config/config_server');
+
+var mongoose = require('mongoose');
+
+var itemSchema = mongoose.Schema({
+  name:           String,
+  description:    String,
+
+  // for defining template items, which are not usable by characters
+  template:       Boolean
+});
+
+// static factory method
+itemSchema.statics.generateFromTemplate(template) {
+  var newItem = new this();
+  newItem.name = template.name;
+  newItem.description = template.description;
+  newItem.template = false;
+  return newItem;
+}
+
+var model = mongoose.model('Item', itemSchema);
+module.exports = model;
